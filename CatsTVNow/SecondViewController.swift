4cc8b14f88534395e08cb88f2c0d6eb165e7df20
Johann Kerr <johannkerr@gmail.com>
2016-12-23 21:51:52 -0500

    Initial Commit

---
+++ CatsTVNow/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CatsTVNow
+//
+//  Created by Johann Kerr on 12/23/16.
+//  Copyright © 2016 Johann Kerr. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
